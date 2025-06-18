b2f808e44e2a2f1b2e6374a10c00610250ab8082
miaolink <167941042+miaolink@users.noreply.github.com>
2025-06-18 11:20:37 +0800

    Update china_ip_split_rsc.py

--- china_ip_split_rsc.py
+++ china_ip_split_rsc.py
@@ -128,7 +128,7 @@ def write_rsc(china_ips, split_size, list_name_prefix, add_mangle, src_address_l
         if add_mangle:
             f.write("/ip firewall mangle\n")
             for list_name in list_names:
-                f.write(f"add chain=prerouting action=mark-routing new-routing-mark={routing_mark} passthrough=no src-address-list={src_address_list} dst-address-list=!{list_name}\n")
+                f.write(f"add chain=prerouting action=mark-routing new-routing-mark={routing_mark} passthrough=yes src-address-list={src_address_list} dst-address-list=!{list_name}\n")
             logger.info(f"添加 {len(list_names)} 条 mangle 规则，src-address-list={src_address_list}, routing-mark={routing_mark}")
 
     logger.info(f"生成 {filename}，包含 {total_ips} 条，拆分为 {total_ips//split_size + 1} 个列表{'，含 mangle 规则' if add_mangle else ''}")
